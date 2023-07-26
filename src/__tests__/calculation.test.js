744ba8dd3d8eca1b1a89d54c1dcb9315708ccca6
Romello Goodman <6355760+romellogoodman@users.noreply.github.com>
2023-07-25 23:21:01 -0400

    pruning the library for v1.0.0

--- src/__tests__/calculation.test.js
+++ src/__tests__/calculation.test.js
@@ -1,15 +1,7 @@
-import { map, pointOfSinWave, polarToCartesian, toDegrees, toRadians } from '../calculation.js';
+import { map, toDegrees, toRadians } from '../calculation.js';
 
 test('map', () => {
   expect(map(1, 0, 10, 0, 100)).toEqual(10);
-});
-
-test('pointOfSinWave', () => {
-  expect(pointOfSinWave(20, 360, 100)).toEqual(34.20201433256687);
-});
-
-test('polarToCartesian', () => {
-  expect(polarToCartesian(200, 200, 200, 90)).toEqual({ x: 200, y: 400 });
 });
 
 test('toDegrees', () => {
--- src/index.js
+++ src/index.js
@@ -1,32 +1,4 @@
-export { heightAlongSinWave, map, polarToCartesian, toDegrees, toRadians } from './calculation.js';
-export {
-  getHSLColor,
-  getPaletteColor,
-  hexToHSL,
-  hslToHex,
-  isolateColorLayer,
-  modulateColorHSL,
-  modulateColorHex,
-  palettes
-} from './color.js';
-export { generateField, generateParticles, moveParticle } from './flow-field';
-export { adjustXY, generateHarmonograph, randomPendulums } from './harmonograph.js';
-export { lerp, lerpColor, lerpColorMulti, linearInterpolation } from './interpolation.js';
-export { fractal, subdivision } from './misc.js';
-export { noise1D, noise2D, noise3D, noise4D, octive2D } from './noise.js';
-export {
-  centroidOfPoints,
-  dist2,
-  distanceBetweenTwoPoints,
-  distanceToSegment,
-  getAngleOfPolygonPoints,
-  getPolygonPoints,
-  getRandomPoints,
-  getSprialPoints,
-  spline,
-  toPoints
-} from './points.js';
-export { PoissonDisk } from './poisson.js';
-// export { generateQtGrid } from './quadtree.js';
-export { getRandom, getRandomWithSeed, randomBias } from './random.js';
-// export { generateVoronoi } from './voronoi.js';
+export * from './calculation';
+export * from './grids';
+export * from './interpolation';
+export * from './noise';
