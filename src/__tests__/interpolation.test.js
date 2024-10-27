297830a5f2a4b010e4bd272dfe5b828083d0f4e0
Romello Goodman <6355760+romellogoodman@users.noreply.github.com>
2024-10-27 14:32:33 -0400

    Refactor to a single file instead of a npm package

--- src/__tests__/interpolation.test.js
+++
@@ -1,5 +0,0 @@
-import { smoothstep } from '../interpolation.js';
-
-test('smoothstep', () => {
-  expect(smoothstep(15, 10, 20)).toEqual(0.5);
-});
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './calculation';
-export * from './grids';
-export * from './interpolation';
-export * from './noise';
